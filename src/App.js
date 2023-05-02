b2ca837c5c79a5a18af414487c97d6e4583ee32f
Ste <n00202054@iadt.ie>
2023-05-03 00:11:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,187 @@
 import logo from './logo.svg';
 import './App.css';
+import SideDrawer from './components/SideDrawer';
+
+import {  createTheme, ThemeProvider, alpha } from '@mui/material/styles';
+
+import { useEffect, useState } from 'react';
+import { CssBaseline, Grid } from '@mui/material';
 
 function App() {
+  
+const [palette, setPalette] = useState(
+  {
+    "name": "Colors",
+    "primary": {
+        "main": "#4c82ed",
+        "light": "#7196e1",
+        "dark": "#0e43ad"
+    },
+    "secondary": {
+      "main": "#7196e1",
+      "light": "#8638d4",
+      "dark": "#5805ab"
+    },
+    "error": {
+      "main": "#FF6767",
+      "light": "#FFA8A8",
+      "dark": "#FF5757"
+    },
+    "warning": {
+      "main": "#FFA954",
+      "light": "#FFBF80",
+      "dark": "#FF9E3D"
+    },
+    "info": {
+      "main": "#70B7FF",
+      "light": "#B4DAFF",
+      "dark": "#57ABFF"
+    },
+    "success": {
+      "main": "#70ff87",
+      "light": "#b4ffc1",
+      "dark": "#57ff73"
+    },
+    "background": {
+        "default": "#121214",
+        "paper": "#2c2d33"
+    },
+    "text": {
+        "primary": "#fff",
+        "secondary": "#cbcbcd",
+        "disabled": ""
+    }
+}
+)
+
+
+const theme = createTheme({
+
+  palette: {
+      border: alpha(palette.text.primary, 0.1),
+      primary: {
+          light: palette.primary.light,
+          main: palette.primary.main,
+          dark: palette.primary.dark
+      },
+      secondary: {
+          light: palette.secondary.light,
+          main: palette.secondary.main,
+          dark: palette.secondary.dark,
+      },
+      error: {
+        light: palette.error.light,
+        main: palette.error.main,
+        dark: palette.error.dark,
+      },
+      warning: {
+        light: palette.warning.light,
+        main: palette.warning.main,
+        dark: palette.warning.dark,
+      },
+      info: {
+        light: palette.info.light,
+        main: palette.info.main,
+        dark: palette.info.dark,
+      },
+      success: {
+        light: palette.success.light,
+        main: palette.success.main,
+        dark: palette.success.dark,
+      },
+      background: {
+          default: palette.background.default,
+          paper: palette.background.paper
+
+      },
+      text: {
+          primary: palette.text.primary,
+          secondary: palette.text.secondary
+      }
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          transition: 'all 5s linear',
+        },
+      },
+    },
+  },
+  components: {
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+           borderColor: '#2b2b2b'
+          }
+        }
+      },
+      MuiList: {
+        styleOverrides: {
+          root: {
+            display: 'flex',
+            flexFlow: "row wrap",
+            justifyContent: 'center',
+            borderRadius: '12px',
+            maxWidth: '200px',
+          }
+        }
+      },
+      MuiTabs: {
+        styleOverrides: {
+          root: {
+            backgroundColor: 'primary.main',
+            display: 'flex',
+            justifyContent: 'space-around',
+            borderRadius: '12px',
+            width: '100%',
+          }
+        }
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          root: {
+            width:'50px', height:'50px', border:'0px solid', borderRadius: '12px'
+          },
+          notchedOutline:{
+            border: 0,
+            
+          },
+        }
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            ":hover": {
+              opacity: 0.9,
+              
+            },
+            fontSize: '16px',
+            paddingTop: '8px',
+            paddingBottom: '8px',
+            paddingLeft: '32px',
+            paddingRight: '32px',
+            borderRadius: '24px'
+          }
+        }
+      }
+    },
+  
+});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <CssBaseline/>
+    <Grid 
+      container
+      maxWidth="xl"
+      columns={12}
+      style={{ minHeight: '100vh' }} 
+      sx={{backgroundColor: 'background.default'}}> 
+      <SideDrawer theme={theme}/>
+    </Grid>
+    
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/SideDrawer.js
@@ -0,0 +1,338 @@
+import { Drawer } from "@mui/material"
+import { alpha } from "@mui/material"
+import { useState } from "react";
+
+
+
+import { AppBar, Grid, Typography } from "@mui/material";
+
+import styled from "@emotion/styled";
+
+import Button from '@mui/material/Button';
+import { AnimatePresence, motion } from "framer-motion";
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import PrimaryPage from "./PrimaryPage";
+import Background from "./Background";
+
+
+
+
+const SideDrawer = ({theme}) => {
+    
+
+    
+    const StyledButton = styled(Button)({
+        '&.MuiButtonBase-root-MuiTab-root,': {
+            '&:hover, &.Mui-hovered': {
+                backgroundColor: theme.palette.background.paper,
+            },
+            '&.Mui-selected': {
+                backgroundColor: theme.palette.background.paper,
+                width: '100%',
+                color: theme.palette.text.primary,
+            },
+            transition: 'ease-in-out 0.2s',
+            width: '100%',
+            alignItems: 'start',
+            textTransform: 'none',
+            color: theme.palette.text.primary,
+            fontSize: '20px',
+            fontWeight: '400',
+            paddingTop: '12px',
+            paddingBottom: '12px',
+            paddingLeft: '24px',
+            paddingRight: '24px',
+            borderRadius: '12px',
+            
+            backgroundColor: theme.palette.background.default,
+            '&:hover, &.Mui-hovered': {
+                transition: 'ease-in-out 0.2s',
+                backgroundColor: theme.palette.background.paper,
+                '@media (hover: none)': {
+                backgroundColor: 'transparent'
+                },
+            },
+        }
+    });
+
+    const [mobileOpen, setMobileOpen] = useState(true);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const [isVisible, setIsVisible] = useState(true);
+        	
+    const [isBackgroundVisible, setIsBackgroundVisible] = useState(false);
+    const [isPrimaryVisible, setIsPrimaryVisible] = useState(false);
+    const [isThemeVisible, setIsThemeVisible] = useState(false);
+    const [isTypographyVisible, setIsTypographyVisible] = useState(false);
+    const [isPageVisible, setPageVisible] = useState(false)
+
+    const [renderController, setRenderController] = useState(true);
+    const [size, setSize] = useState('h4');
+    const [title, setTitle] = useState('Controller');
+    const [color, setColor] = useState('text.primary');
+
+    const handleText = (size, title, color) => {
+        setSize(size)
+        setTitle(title)
+        setColor(color)
+    }
+
+    let easeEntry = [0.8, 0.1, 0.2, 0.9]
+    let easeExit = [0.71, 0.03, 0.56, 0.95]
+
+    let marginLeft = 0;
+    if(renderController){
+        marginLeft = 3
+    }
+    
+    let pointerBool = 'pointer';
+    if(renderController){
+        pointerBool = 'cursor'
+    }
+
+    
+    return (
+        <>
+             {/* Mobile Drawer */}                    
+             <Drawer
+                PaperProps={{
+                    sx: {
+                        backgroundColor: alpha(theme.palette.background.default, 0),
+                        borderRight: '1px solid',
+                        borderColor: 'border',
+                        opacity: 1,
+                        backdropFilter: "blur(64px)", 
+                    }
+                }}
+                sx={{
+                    
+                    display: {xs: 'block', md: 'none' },
+                    justifyContent: 'center',
+                    '& .MuiDrawer-paper': { width: '100%' },
+                    }}
+                variant="temporary"
+                open={mobileOpen}
+                disableBackdropTransition
+                onClose={handleDrawerToggle}
+                anchor="left"
+            >
+
+                {/* APP BAR */}
+                <AppBar 
+                    position="sticky" 
+                    elevation={0}
+                    sx={{zIndex: '1000', backgroundColor: alpha(theme.palette.background.default, 0), backdropFilter: "blur(64px)", }} >
+                    <Grid sx={{position: 'absolute', width: '100%'}}>
+                    
+                    {
+                        isPageVisible && (
+                        <motion.div
+                        layout
+                        style={{width: '100%', zIndex: '1501', position: ''}}
+                        initial={{  x: '50%'}}
+                        animate={{
+                            x:0,
+                            transition: { duration: 0.3, ease: easeEntry},
+                        }}
+                        exit={{
+                            x: '100%',
+                            transition: { duration: 0.2, ease: easeExit },
+                        }}
+                        >
+                        <Typography align="center" sx={{zIndex: '1501', my:1, fontWeight: ''}}  color={"text.primary"} variant={"subtitle1"} >
+                            {title}
+                        </Typography> 
+                        </motion.div>
+                        )
+                        
+                    }
+                
+                    
+                    </Grid>
+
+                    <Grid sx={{backgroundColor: 'inherit', position: 'sticky'}}>
+
+                        {/* Controller */}
+
+                        <motion.div
+                        layout
+                    
+                        transition={{layout: { duration: 0.3, ease: easeEntry,}}}
+                        layoutId="text" style={{zIndex: '1500'}}>
+                        
+
+                            <Grid sx={{display: 'flex', alignItems: 'center'}} >
+
+                                
+                                <Grid sx={{display: 'flex', alignItems: 'center', cursor: pointerBool, width: '100%'}}
+                                    onClick={() => {
+                                        if(isPageVisible){
+                                        setRenderController(true) 
+                                        setIsBackgroundVisible(false)
+                                        setIsPrimaryVisible(false)
+                                        setPageVisible(false)
+                                        setIsThemeVisible(false)
+                                        setIsTypographyVisible(false)
+                                        handleText('h4', 'Controller', 'text.main')
+                                        } 
+                                    
+                                        
+                                    }}
+                                >
+                                    { 
+                                        isPageVisible && (
+                                            <>
+                                                <Grid sx={{display: 'flex',  alignItems: 'center'}}>
+                                                    <ArrowBackIosIcon sx={{my:1, ml: 3}} fontSize="xs" color="primary"/> 
+                                                </Grid>
+                                            </>
+                                        )
+                                        
+                                    }
+                                
+                                    <Grid sx={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+                                        <Typography sx={{my:1, ml: marginLeft, }} color={color} variant={size}>
+                                            Settings
+                                        </Typography>
+                                        
+                                        {
+                                            renderController && (
+                                                <IconButton
+                                                sx={{mr:3}}
+                                                    disableRipple
+                                                    color="inherit"
+                                                    aria-label="open drawer"
+                                                    edge="start"
+                                                    onClick={() => {
+                                                        setMobileOpen(false)
+                                                    }}
+                                                >
+                                                    <CloseIcon />
+                                                </IconButton>
+                                            )
+                                        }
+                                    </Grid>
+                                                                
+                                </Grid>
+
+                            </Grid>
+
+                        </motion.div>
+                    
+                    </Grid>
+                </AppBar>
+                <Grid sx={{width: '100%',  display: 'flex'}} >
+            
+            
+                <Grid sx={{ position: 'absolute', p:3, width: '100%', backdropFilter: "blur(64px)", }}>
+
+                
+                <AnimatePresence >
+                {renderController && (
+                        
+                    <motion.div
+                        layout
+                        key="Codn"
+                        initial={{opacity: 0, scale: 0.8, x: -100 }}
+                        animate={{
+                            x: 0,
+                            scale: 1,
+                            opacity: 1,
+                            transition: { duration: 0.3, ease: easeEntry},
+                          }}
+                          exit={{
+                            scale: 0.8,
+                            x: -50,
+                            opacity: 0.3,
+                            transition: { duration: 0.2, ease: easeExit, opacity: 0.2  },
+                          }}
+                        transition={{layout: { duration: 0.3, ease: easeExit,}}}
+                    >
+                       
+
+                        
+                        
+
+
+                        {/* Background */}
+                        <StyledButton 
+                            sx={{width: '100%'}}
+                            disableRipple
+                            onClick={() => {
+                                setIsBackgroundVisible(true)  
+                                setRenderController(false)
+                                handleText('subtitle1', 'Wi-Fi', 'primary.main')
+                                setPageVisible(true)                                
+                                
+                            }}
+                            >
+                                Wi-Fi
+                        </StyledButton>
+
+
+                        {/* Primary */}
+                        <StyledButton 
+                            disableRipple
+                            sx={{width: '100%', mt: 3}}
+                            onClick={() => {
+                                setIsPrimaryVisible(true) 
+                                setRenderController(false)
+                                handleText('subtitle1', 'Primary', 'primary.main')
+                                setPageVisible(true)                             
+                            }}
+                            >
+                                Primary
+                        </StyledButton>
+
+
+                    </motion.div>
+                   )}
+                   </AnimatePresence>
+                </Grid>
+                
+              
+                <Background isBackgroundVisible={isBackgroundVisible}/>
+
+                <PrimaryPage isPrimaryVisible={isPrimaryVisible}/>
+          
+                
+                <AnimatePresence>
+                {isThemeVisible && (
+               
+                        <motion.div 
+                            sx={{width: '100%'}}
+                            style={{width: '100%'}}
+                            initial={{ x: "100%" ,  scale: 0.8}}
+                            animate={{
+                                x: 0,
+                                scale: 1,
+                                transition: { duration: 0.3, ease: easeEntry},
+                            }}
+                            exit={{
+                                x: "100%",
+                                opacity: 0,
+                                transition: { duration: 0.2, ease: [0.1, 0.8, 0.9, 0.2], opacity: 0.2 },
+                            }}
+                            
+                        
+                        > 
+                        <p>theme accordian</p>
+                    </motion.div>
+          
+                )}
+                </AnimatePresence>
+            
+            </Grid>
+                   
+              
+            </Drawer> 
+        </>
+    )
+
+}
+export default SideDrawer
